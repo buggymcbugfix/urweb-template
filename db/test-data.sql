0f46cf90ddccb7d8abe4439e65520ef4668cb8f6
Vilem Liepelt <17603372+buggymcbugfix@users.noreply.github.com>
2026-07-25 12:02:08 +0200

    More convenient adding of test data

--- db/test-data.sql
+++ db/test-data.sql
@@ -7,16 +7,35 @@ INSERT INTO User_tbl
 	(     3 , 'Louie' )
 ;
 
-INSERT INTO Txn_tbl
-	(UserId , TxnUserSeq , EffectiveDate , Timestamp         , Delta  , Balance , Category     , Description           ) VALUES
-	(     1 ,          1 , '2026-06-01'  , CURRENT_TIMESTAMP ,  10000 ,   10000 , 'Adjustment' , 'Opening balance'     ),
-	(     1 ,          2 , '2026-06-02'  , CURRENT_TIMESTAMP ,   1000 ,   11000 , 'Bonus'      , 'Chores'              ),
-	(     1 ,          3 , '2026-06-03'  , CURRENT_TIMESTAMP , -  395 ,   10605 , 'Withdrawal' , 'Wants to buy a comic'),
-	(     2 ,          1 , '2026-06-01'  , CURRENT_TIMESTAMP ,   1000 ,    1000 , 'Adjustment' , 'Opening balance'     ),
-	(     3 ,          1 , '2026-06-01'  , CURRENT_TIMESTAMP ,   1000 ,    1000 , 'Adjustment' , 'Opening balance'     )
+CREATE VIEW Txn_vw AS
+	SELECT UserId, EffectiveDate, Delta, Category, Description
+	FROM Txn_tbl
+;
+
+CREATE TRIGGER Txn_vw_insert
+INSTEAD OF
+INSERT ON Txn_vw
+BEGIN
+	INSERT INTO Txn_tbl
+		(UserId    , UserTxnSeq                                                                      , EffectiveDate    , Timestamp        , Delta    , Balance                                                                                                                                                          , Category    , Description     ) VALUES
+		(NEW.UserId, COALESCE((SELECT MAX(UserTxnSeq) + 1 FROM Txn_tbl WHERE UserId = NEW.UserId), 1), NEW.EffectiveDate, CURRENT_TIMESTAMP, NEW.Delta, COALESCE((SELECT Balance FROM Txn_tbl WHERE UserId = NEW.UserId AND UserTxnSeq = (SELECT MAX(UserTxnSeq) FROM Txn_tbl WHERE UserId = NEW.UserId)), 0) + New.Delta, NEW.Category, NEW.Description )
+	;
+END
+;
+
+INSERT INTO Txn_vw
+	(UserId , EffectiveDate , Delta  , Category     , Description ) VALUES
+	(     1 , '2026-06-01'  ,  10000 , 'Adjustment' , 'Opening balance' ),
+	(     1 , '2026-06-02'  ,   1000 , 'Bonus'      , 'Chores' ),
+	(     1 , '2026-06-03'  , -  395 , 'Withdrawal' , 'Wants to buy a comic'),
+	(     2 , '2026-06-01'  ,   1000 , 'Adjustment' , 'Opening balance' )
+;
+
+DROP VIEW Txn_vw
 ;
 
 INSERT INTO PocketMoney_weeklyAmount
 	(UserId, Amount, Starting    ) VALUES
-	(     1,    500, '2025-12-12')
+	(     1,    500, '2025-12-12'),
+	(     2,    500, '2025-12-12')
 ;
